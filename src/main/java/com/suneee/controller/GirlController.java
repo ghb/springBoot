6819679f37fc0c6e9e9510378c4128d90f0480af
Evans <408806001@qq.com>
2017-03-21 14:02:17 +0800

    2017-03-21 Spring Boot Test Girl Project update GirlController

--- src/main/java/com/suneee/controller/GirlController.java
+++ src/main/java/com/suneee/controller/GirlController.java
@@ -19,6 +19,7 @@ import java.util.List;
  */
 @RestController
 public class GirlController {
+    //日志
     private static final Logger logger = LoggerFactory.getLogger(GirlController.class);
 
     @Autowired
